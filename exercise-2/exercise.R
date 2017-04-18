c2a22b400c1715cc1d0f3a0e068fa72cdf2124af
Joseph Chou <joseph98c@gmail.com>
2017-04-18 13:49:17 -0700

    completed exercise 2

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -24,15 +24,17 @@ vehicles.1997 <- filter(vehicles, year == 1997)
 only.one.year <- length(unique(vehicles.1997$year)) == 1
 
 # Create a data.frame of 2-Wheel Drive vehicles that get more than 20 miles/gallon in the city
-vehicles.2wd <- filter(vehicles, drive == '2-Wheel Drive' & cty > 20)
+vehicles.2wd <- filter(vehicles, drive == '2-Wheel Drive', cty > 20)
 
 # Of those vehicles, what is the vehicle ID of the vehicle with the worst hwy mpg?
-worst.hwy <- arrange(vehicles.2wd, hwy)$id[1]
+worst.hwy <- select(filter(vehicles.2wd, hwy == min(hwy)), id)
 
 # Write a function that takes a `year` and a `make` as parameters, and returns 
 # The vehicle that gets the most hwy miles/gallon of vehicles of that make in that year
 MostHwyMPG <- function(my.year, my.make) {
-  return (arrange(filter(vehicles, year == my.year & make == my.make), desc(hwy))$model[1])
+  filtered <- filter(vehicles, year == my.year, make == my.make)
+  filtered <- filter(filtered, hwy == min(hwy))
+  return (select(filtered, model))
 }
 
 # What was the most efficient honda model of 1995?
